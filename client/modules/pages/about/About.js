87b318873afca2833204bcdefe28231dc1b9cf33
Cody Brunner <rockchalkwushock@icloud.com>
2017-01-30 07:06:09 -0600

    about page set.

--- client/modules/pages/about/About.js
+++ client/modules/pages/about/About.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import { Card, Image } from 'semantic-ui-react';
+
+const src = 'http://funnyasduck.net/wp-content/uploads/2012/11/funny-darth-vader-drinking-tea-dark-side-star-wars-great-pics.jpg';
+const description = [
+  'Hello World!',
+  'Doing it BIG!',
+  'All day every day!'
+].join(' ');
 
 const About = () => (
   <div className="aboutpage">
-    This is the About Page.
+    <Image src={src} size='small' shape='circular' />
+    <Card centered raised>
+      <Card.Content description={description} />
+    </Card>
   </div>
 );
 

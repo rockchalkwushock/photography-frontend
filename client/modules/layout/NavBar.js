e5e876a3bdecf187a8a3a5213bb7c9fbc64db02b
Cody Brunner <rockchalkwushock@icloud.com>
2017-01-30 09:31:31 -0600

    Added Language Dropdown to NavBar. Updated NavBar view. Updated NavBar style. Updated locale message list.

--- client/modules/layout/NavBar.js
+++ client/modules/layout/NavBar.js
@@ -1,11 +1,12 @@
 import React, { PropTypes } from 'react';
 import { browserHistory } from 'react-router/es';
-import { withTranslate } from 'react-redux-multilingual';
-import { Menu } from 'semantic-ui-react';
-
+import { IntlActions, withTranslate } from 'react-redux-multilingual';
+import { Dropdown, Menu } from 'semantic-ui-react';
+import { store } from '../../redux/store';
 
 const NavBar = ({ translate }) => (
-  <Menu borderless>
+  <Menu borderless className='nav'>
+    {/* HOME BUTTON */}
     <Menu.Menu>
       <Menu.Item
         onClick={() => browserHistory.push('/')}
@@ -13,11 +14,31 @@ const NavBar = ({ translate }) => (
         {translate('home-btn')}
       </Menu.Item>
     </Menu.Menu>
-    <Menu.Menu position='right'>
+    {/* ABOUT BUTTON */}
+    <Menu.Menu>
       <Menu.Item
         onClick={() => browserHistory.push('/about')}
       >
         {translate('about-btn')}
+      </Menu.Item>
+    </Menu.Menu>
+    {/* LANGUAGE MENU */}
+    <Menu.Menu position='right'>
+      <Menu.Item>
+        <Dropdown item text={translate('lang')}>
+          <Dropdown.Menu>
+            <Dropdown.Item
+              flag='ru'
+              text={translate('russian')}
+              onClick={() => store.dispatch(IntlActions.setLocale('ru'))}
+            />
+            <Dropdown.Item
+              flag='us'
+              text={translate('english')}
+              onClick={() => store.dispatch(IntlActions.setLocale('en'))}
+            />
+          </Dropdown.Menu>
+        </Dropdown>
       </Menu.Item>
     </Menu.Menu>
   </Menu>

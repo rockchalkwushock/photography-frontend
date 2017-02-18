32b758b9a820bd9519094d145a0e68dc45ffa46c
Cody Brunner <rockchalkwushock@icloud.com>
2017-02-18 08:39:00 -0600

    Updated eslintrc. Updated gitignore. Updated entry point. Set layout of Footer.

--- client/index.js
+++ client/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { AppContainer } from 'react-hot-loader';
+import 'font-awesome/css/font-awesome.css';
 import Root from './Root';
 import { store } from './redux';
 import './styles/styles.css';
--- client/layout/Footer.js
+++ client/layout/Footer.js
@@ -1,8 +1,44 @@
 import React from 'react';
+import { LegalBar, SocialBar } from '../commons';
 
 const Footer = () => (
   <div className="footer">
-    <h1>This is the Footer.</h1>
+    <SocialBar>
+      <a
+        className='icon'
+        target='_blank'
+        rel='noreferrer noopener'
+        href='https://www.facebook.com/'
+        title='facebook'
+      >
+          <i className="fa fa-facebook-square fa-2x"></i>
+      </a>
+      <a
+        className='icon'
+        target='_blank'
+        rel='noreferrer noopener'
+        href='https://www.instagram.com/?hl=ru'
+        title='instagram'
+      >
+          <i className="fa fa-instagram fa-2x"></i>
+      </a>
+      <a
+        className='icon'
+        target='_blank'
+        rel='noreferrer noopener'
+        href='mailto:mashaeltsova@mail.ru'
+        title='email'
+      >
+          <i className="fa fa-envelope fa-2x"></i>
+      </a>
+    </SocialBar>
+    <LegalBar>
+      <span>
+        <i className="fa fa-copyright"></i>
+        2017 RCWS Development.
+      </span>
+
+    </LegalBar>
   </div>
 );
 

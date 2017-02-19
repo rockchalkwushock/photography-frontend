4165a50c32d8d208fd939412f2b7ad509416042f
Cody Brunner <rockchalkwushock@icloud.com>
2017-02-19 12:30:40 -0600

    updated store with thunk.

--- client/redux/index.js
+++ client/redux/index.js
@@ -1,11 +1,12 @@
 import { applyMiddleware, createStore, compose } from 'redux/es';
+import thunk from 'redux-thunk';
 import { syncHistoryWithStore, routerMiddleware } from 'react-router-redux';
 import { browserHistory } from 'react-router/es';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './reducers';
 
 const routingMiddleware = routerMiddleware(browserHistory);
-const middlewares = [routingMiddleware];
+const middlewares = [routingMiddleware, thunk];
 let enhancers;
 
 if (process.env.NODE_ENV !== 'production') {

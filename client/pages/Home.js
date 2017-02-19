3bc0467c430ac05d979feffad10d15d54661dd45
Cody Brunner <rockchalkwushock@icloud.com>
2017-02-19 12:29:00 -0600

    working on redux for carousel.

--- client/pages/Home.js
+++ client/pages/Home.js
@@ -1,18 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { browserHistory } from 'react-router/es';
 import { Card } from '../commons';
 
-const Home = () => (
-  <div className="homepage">
-    <div className="top">
-      <Card onClick={() => browserHistory.push('/portrait/family')} />
-      <Card />
-    </div>
-    <div className="bottom">
-      <Card />
-      <Card />
-    </div>
-  </div>
-);
+class Home extends Component {
+  _onClick(collection) {
+    this.props.selectCollection(collection);
+    browserHistory.push(`/portrait/${collection}`);
+  }
+  render() {
+    return (
+      <div className="homepage">
+        <div className="top">
+          <Card key='family' onClick={this._onClick()} />
+          <Card />
+        </div>
+        <div className="bottom">
+          <Card />
+          <Card />
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Home;

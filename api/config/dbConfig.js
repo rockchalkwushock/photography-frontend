e3fb30e4d4ecf2521f43394aa45a9184047080e3
Cody Brunner <rockchalkwushock@icloud.com>
2017-02-20 13:58:31 -0600

    Updated console.logs

--- api/config/dbConfig.js
+++ api/config/dbConfig.js
@@ -10,6 +10,6 @@ export default (conf) => {
   mongoose.Promise = global.Promise;
   mongoose.connect(conf);
   mongoose.connection
-    .once('open', () => console.log(`Connected to MongoDb`)) // eslint-disable-line
+    .once('open', () => console.log(`Connected to MongoDb: ${conf}`)) // eslint-disable-line
     .on('error', err => console.warn('Warning', err)); // eslint-disable-line
 };
--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import { join } from 'path';
 import { dbConfig, middlewaresConfig } from './config';
 
 const app = express();
+const MODE = process.env.NODE_ENV;
 
 const PORT = process.env.PORT || 3000;
 
@@ -10,16 +11,16 @@ middlewaresConfig(app);
 
 // TODO: app.use('/api/v1', PostRoutes);
 
-if (process.env.NODE_ENV !== 'production') {
+if (MODE !== 'production') {
   // development environment
-  const mongoConf = 'mongodb://localhost/dev-db';
+  const mongoConf = 'mongodb://localhost/dev';
   dbConfig(mongoConf);
 } else {
   app.use(express.static('dist'));
   app.get('*', (req, res) => {
     res.sendFile(join(__dirname, '../dist/index.html'));
   });
-  const mongoConf = process.env.MONGODB;
+  const mongoConf = process.env.MONGODB || 'mongodb://localhost/prod';
   if (!mongoConf) {
     throw new Error('Error with Mongodb Process');
   } else {
@@ -29,5 +30,5 @@ if (process.env.NODE_ENV !== 'production') {
 
 app.listen(PORT, err => {
   if (err) { return console.error(err); }
-  console.log(`App running to port: ${PORT}`);
+  console.log(`App running on port: ${PORT} in ${MODE} mode.`);
 });

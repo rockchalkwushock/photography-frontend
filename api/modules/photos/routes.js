57763b51d9ffbcb61b8bdc61042d09beb4d0f4fd
Cody Brunner <rockchalkwushock@icloud.com>
2017-02-21 09:41:36 -0600

    Added photoRoutes.

---
+++ api/modules/photos/routes.js
@@ -0,0 +1,7 @@
+import { Router } from 'express';
+import { PhotoApi } from './controller';
+
+const photoApi = new PhotoApi();
+const routes = new Router();
+
+routes.route('/collection/:name/:url').post(photoApi.createPhoto);

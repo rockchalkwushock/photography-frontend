ff975cd969b21b0a0caecdb343ec273d065b8db9
Cody Brunner <rockchalkwushock@icloud.com>
2017-02-21 09:41:12 -0600

    updated routing. added post route.

--- api/modules/collections/routes.js
+++ api/modules/collections/routes.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
-import * as collectionController from './controller';
+import { CollectionApi } from './controller';
 
+const collectionApi = new CollectionApi();
 const routes = new Router();
 
-routes.route('/collection/:name').get(collectionController.fetchCollectionByName);
+routes.route('/collection/:name').post(collectionApi.createCollection);
+routes.route('/collection/:name').get(collectionApi.fetchCollectionByName);
 
 export default routes;

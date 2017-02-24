6b356c234451e4bbf3b9d4ac0abf63d9ceac48f1
Cody Brunner <rockchalkwushock@icloud.com>
2017-02-24 12:14:09 -0600

    Added & Updated api routing.

--- api/modules/categories/routes.js
+++ api/modules/categories/routes.js
@@ -4,8 +4,9 @@ import { CategoryApi } from './controller';
 const collectionApi = new CategoryApi();
 const routes = new Router();
 
-// routes.route('/category').get(collectionApi.fetchCategories);
+routes.route('/category').get(collectionApi.fetchCategories);
 routes.route('/category').post(collectionApi.createCategory);
 routes.route('/category/:name').get(collectionApi.fetchCategoryByName);
+routes.route('/category/:name').post(collectionApi.addPhotoToCategory);
 
 export default routes;
